e2ed5a3916868e0a9be7fa9705daacb69aa2e4a8
Tobias Treppmann <hello@treppmann.design>
2026-01-06 20:07:04 +0100

    Configure custom domain treppmann.design

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  site: "https://oratioandco.github.io",
-  base: "/treppmanndesignportfolio",
+  site: "https://treppmann.design",
+  base: "/",
 });
