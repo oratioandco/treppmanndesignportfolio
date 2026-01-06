29c7e9c9466c19cb02381d7fd0c35b3d4981e7ca
Tobias Treppmann <hello@treppmann.design>
2026-01-06 19:28:11 +0100

    Update site config for oratioandco org

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  site: "https://tobystereo.github.io",
-  base: "/portfolio",
+  site: "https://oratioandco.github.io",
+  base: "/treppmanndesignportfolio",
 });
